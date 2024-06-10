8ca737351b950b3da831683d65c89a9f4e4b07ac
Pc8spN42 <j_jmf@hotmail.com>
2024-06-10 08:06:36 +0200

    add files

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/***/
+/**
+ * The saml backend module for the auth plugin.
+ *
+ * @packageDocumentation
+ */
+
+export { authModuleSaml as default } from './module';
---
+++ src/module.ts
@@ -0,0 +1,74 @@
+import {
+  coreServices,
+  createBackendModule,
+} from '@backstage/backend-plugin-api';
+import { authProvidersExtensionPoint } from '@backstage/plugin-auth-node';
+import { saml } from './provider';
+
+// use by Create entity for user authorized
+import { DEFAULT_NAMESPACE, stringifyEntityRef } from '@backstage/catalog-model';
+
+
+export const authModuleSaml = createBackendModule({
+  pluginId: 'auth',
+  moduleId: 'saml',
+  register(reg) {
+    reg.registerInit({
+      deps: { 
+        logger: coreServices.logger,
+        providers: authProvidersExtensionPoint,
+      },
+      async init({ logger, providers }) {
+        
+        providers.registerProvider({
+          providerId: 'saml',
+          factory: saml.create({
+            signIn: {
+              resolver: async (info, ctx) => {
+                logger.info("DDDDDDDDDDDDDDDDDDDDDDDDDDD")
+                const {
+                  profile: { email },
+                } = info;
+                if (!email) {
+                  throw new Error('Profile contained no email');
+                }
+                
+                
+                // Create entity for user authorized
+                const userId = email.split('@')[0];
+                const entityRef = stringifyEntityRef({
+                  kind: 'User',
+                  namespace: DEFAULT_NAMESPACE,
+                  name: userId,
+                });
+                const token = await ctx.issueToken({
+                  claims: {
+                    sub: entityRef,
+                    ent: [entityRef],
+                  },
+                });
+                return token;
+                // End
+
+                // Log in if exist in catalog
+                // const [name] = email.split('@');
+                // return ctx.signInWithCatalogUser({
+                //   entityRef: { name },
+                // });
+                // End
+                
+              },
+            },
+            
+            
+          }),
+                    
+        });
+        
+
+
+      },
+    });
+  },
+});
+
